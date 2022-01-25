eb3bcbbf24a26448d43ec9683bb449cb37cf4328
anchounio <toni.clemente.garcia@gmail.com>
2022-01-25 18:08:08 +0100

    Ready for testing

---
+++ function.js
@@ -0,0 +1,5 @@
+export function strictEquals(a, b) {
+    const result = a + b;
+    console.log(result);
+    return result;
+}
---
+++ testing.js
@@ -0,0 +1,3 @@
+import { strictEquals } from "./function.js";
+
+strictEquals(1, 2);
